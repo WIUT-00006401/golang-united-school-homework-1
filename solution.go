b0b547acbae8f1882b36258547bf380308503067
WIUT_00006401 <durdona.jurayeva@mail.ru>
2022-12-29 23:25:32 +0500

    Fixed

--- solution.go
+++ solution.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji/v2"
 )
 
-func main() {
-
-	message := emoji.Sprint("Hello :world_map:!")
-	fmt.Println(message)
+func GetMessage() string {
+	return emoji.Sprint("Hello :world_map:!")
 }
